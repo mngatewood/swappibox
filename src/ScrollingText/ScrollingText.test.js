45de763676eca9882a099b128a75ed7c3ecb9b0c
Michael Gatewood <michael@mngatewood.com>
2018-03-19 13:57:27 -0600

    First commit

---
+++ src/ScrollingText/ScrollingText.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import ScrollingText from './ScrollingText';
+
+describe('ScrollingText', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ScrollingText />);
+  });
+
+  it('should match the snapshot', () => {
+
+  });
+
+  it('should match the snapshot', () => {
+
+  });
+
+});
